68279815d1ce40dde391635ad8438fdac5bb0304
Dresch07 <duardodresch007@gmail.com>
2025-04-16 00:58:38 +0000

    terminado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,50 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int matriz[10][10] = { };
+    char colunas[10] = {'A','B','C','D','E','F','G','H','I','J'};
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    for (int i = 0; i < 5; i++) {
+    matriz[i][3] = 3;  
+    matriz[2][i+1]= 3;
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+}      
+    for (int i=1;i<4;i++){
+    matriz [5][2]=2 ;
+    matriz [6][i]=2;
+}
+    for (int i =0; i < 5; i++) {
+    matriz[7][i] = 2;     
+}      
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+
+    for (int i =0; i < 10; i++) {
+    matriz[3][7] = 1;  matriz[7][7] = 1;
+ }   
+    for (int i =6; i < 9; i++) {
+    matriz[4][i] = 1;
+    matriz[6][i] = 1;
+ }   
+    for (int i =5; i < 10; i++) {
+    matriz[5][i] = 1;  
+ }   
+    
+       
+    printf("   ");
+    
+    for (int j = 0; j < 10; j++) {
+        printf("%c ", colunas[j]);
+    }
+    printf("\n");
+
+    
+    for (int i = 0; i < 10; i++) {
+        printf("%2d ", i + 1); 
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
